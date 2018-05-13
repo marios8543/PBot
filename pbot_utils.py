8d657e6bdb3cbf86038198659c2436b6bec7f11f
marios <marios8543@gmail.com>
2018-05-13 20:01:28 +0300

    Added reason to votekick

--- pbot_utils.py
+++ pbot_utils.py
@@ -228,4 +228,4 @@ class Utils():
                         
     def delete_server(id):
         db.delete(table='servers',params={'id':id})
-        db.delete(table='members',params={'server_id':id})                        
+        db.delete(table='members',params={'server_id':id})
