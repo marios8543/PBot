b318b2db27b690b251ba51414fd2b6b8c973da1c
marios <marios8543@gmail.com>
2018-05-18 19:08:29 +0300

    Merge branch 'aiomysql-wip'
    Got rid of the blocking mysql connector after months lol

--- pbot_utils.py
+++ pbot_utils.py
@@ -205,8 +205,6 @@ class Utils():
         user = server.get_member(ctx.message.author.id)
         return getattr(user.server_permissions,perm)
         
-
-<<<<<<< HEAD
     def get_server(id):
         result = db.select(table='servers',fields=[
         'added_on','entry_text','entry_text_pm','goodbye_text',
@@ -245,7 +243,7 @@ class Utils():
     def delete_server(id):
         db.delete(table='servers',params={'id':id})
         db.delete(table='members',params={'server_id':id})
-=======
+        
     async def make_server(id=0):
         await db.insert(table='servers',values={'id':id})
         return await Utils.get_server(id)
@@ -253,4 +251,3 @@ class Utils():
     async def delete_server(id):
         await db.delete(table='servers',params={'id':id})
         await db.delete(table='members',params={'server_id':id})
->>>>>>> aiomysql-wip
